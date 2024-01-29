9baae959e56ea504cffef56549422279effe838e
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-01-29 09:16:33 +0300

    mailer and logo

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,1 +1,12 @@
 const nodemailer = rquire("nodemailer");
+const Mailgen = require("mailgen");
+
+const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+        name: "MindBridge",
+        link: "https://sites.google.com/view/bse24-10/home",
+        // Optional product logo
+        logo: "../assets/images/MindBridgeLogo.jpeg"
+    },
+});
