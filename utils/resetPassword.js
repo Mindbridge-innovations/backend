df6d33b59e2e462b632885c6116b0cfccec21d82
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-02-12 12:38:27 +0300

    refractoring and handling errors

--- utils/resetPassword.js
+++ utils/resetPassword.js
@@ -1,11 +1,12 @@
 // utils/resetPassword.js
-const bcrypt = require('bcryptjs');
 const { getDatabase, ref, set, get } = require('firebase/database');
 const { firebaseApp } = require('./firebaseConfig');
 const { hashPassword } = require('./hash');
 
 const resetPassword = async (email, token, newPassword) => {
   const db = getDatabase(firebaseApp);
+  
+  // Reference to the password reset token in the database
   const tokenRef = ref(db, `passwordResetTokens/${email.replace(/\./g, ',')}`);
   const tokenSnapshot = await get(tokenRef);
   const tokenData = tokenSnapshot.val();
@@ -14,11 +15,19 @@ const resetPassword = async (email, token, newPassword) => {
     throw new Error('Invalid or expired password reset token');
   }
 
+  // Reference to the user in the database
+  const userRef = ref(db, `users/${email.replace(/\./g, ',')}`);
+  const userSnapshot = await get(userRef);
+  const userData = userSnapshot.val();
+
+  if (!userData) {
+    throw new Error('User not found');
+  }
+
   // Hash the new password
   const hashedPassword = await hashPassword(newPassword);
 
   // Update the user's password in the database
-  const userRef = ref(db, `users/${email.replace(/\./g, ',')}`);
   await set(userRef, { ...userData, password: hashedPassword });
 
   // Delete the password reset token from the database
