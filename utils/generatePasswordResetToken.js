fc61afc89304b57ce2c2476925b0a8d1acb2c950
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-02-12 11:25:16 +0300

    Update generatePasswordResetToken.js

--- utils/generatePasswordResetToken.js
+++ utils/generatePasswordResetToken.js
@@ -1,3 +1,5 @@
 //utils/generatePasswordResetToken.js
 const crypto = require('crypto');
-const{getDatabse, ref,set,get} = require('firebase/database')
+const{getDatabse, ref,set,get} = require('firebase/database');
+const {firebaseApp} = require('./firebaseConfig');
+const sendEmail
