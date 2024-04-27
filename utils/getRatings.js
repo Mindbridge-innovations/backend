edd1b95e527a1b8ba59c4484b7c23d9149bf92dd
Mamgbi Geofrey <gmamgbi1999@gmail.com>
2024-04-27 09:33:36 +0300

    refining rating querry

--- utils/getRatings.js
+++ utils/getRatings.js
@@ -7,24 +7,11 @@ const getRatingsAndClientDetails = async (therapistId) => {
     const ratingsSnapshot = await ratingsRef.orderByChild('therapistId').equalTo(therapistId).once('value');
     const ratings = ratingsSnapshot.val();
   
-    if (!ratings) {
-      console.log("No ratings found for this therapist.");
-      return [];
-    }
-  
-    console.log("Ratings found:", ratings);
-  
     const clientDetailsPromises = Object.keys(ratings).map(async (key) => {
       const clientId = ratings[key].clientId;
     //   console.log("Fetching details for clientId:", clientId);
       const clientSnapshot = await usersRef.orderByChild('userId').equalTo(clientId).once('value');
       const clientDetails = clientSnapshot.val();
-  
-      if (!clientDetails) {
-        console.log(`No details found for client with ID: ${clientId}`);
-      } else {
-        console.log(`Details found for client ${clientId}:`, clientDetails);
-      }
   
       return {
         rating: ratings[key],
