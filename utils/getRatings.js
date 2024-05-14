11a20b2e2453b0a0aa7da08657aad27a19150803
Mamgbi Geofrey <gmamgbi1999@gmail.com>
2024-05-14 12:46:52 +0300

    reconfiguring therapist rating query

--- utils/getRatings.js
+++ utils/getRatings.js
@@ -6,20 +6,24 @@ const getRatingsAndClientDetails = async (therapistId) => {
     const usersRef = db.ref('users');
     const ratingsSnapshot = await ratingsRef.orderByChild('therapistId').equalTo(therapistId).once('value');
     const ratings = ratingsSnapshot.val();
-  
+
+    if (!ratings) {
+        return []; // Return an empty array if no ratings are found
+    }
+
     const clientDetailsPromises = Object.keys(ratings).map(async (key) => {
       const clientId = ratings[key].clientId;
       const clientSnapshot = await usersRef.child(clientId).once('value');
       const clientDetails = clientSnapshot.val();
-  
+
       return {
         rating: ratings[key],
         clientDetails,
       };
     });
-  
-    const ratingsAndClients = await Promise.all(clientDetailsPromises);
-    return ratingsAndClients.reverse;
-  };
 
-  module.exports={getRatingsAndClientDetails};
+    const ratingsAndClients = await Promise.all(clientDetailsPromises);
+    return ratingsAndClients.reverse(); // Correctly call reverse()
+};
+
+module.exports = { getRatingsAndClientDetails };
