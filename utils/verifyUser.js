5df3c3babad8da3c9a8676b176402e1f5bf45f87
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-05-25 14:18:00 +0300

    send approval process email after verification

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -1,5 +1,6 @@
 // utils/verifyUser.js
 const { admin, db } = require('./firebaseConfig');
+const sendApprovalProcessEmail = require('./sendApprovalEmail');
 
 const verifyUser = async (token) => {
   const usersRef = db.ref('users');
@@ -10,9 +11,15 @@ const verifyUser = async (token) => {
     const usersData = snapshot.val();
     const userKey = Object.keys(usersData).find(key => usersData[key].verificationToken === token);
     const userRef = db.ref(`users/${userKey}`);
-
+    const userData = usersData[userKey];
+    
     // Update the isVerified field to true and remove the verificationToken
     await userRef.update({ isVerified: true, verificationToken: null });
+
+    // Check if the user is a therapist and send them an email about the approval process
+    if (userData.role === 'therapist') {
+      await sendApprovalProcessEmail(userData.email, userData.firstName);
+    }
 
     return { verified: true, userKey };
   } else {
