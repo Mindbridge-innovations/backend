ef49e6dae8aff1b15fe33a0d2cc809f170649161
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-06-04 18:12:23 +0300

    swagger base api documentation

---
+++ swagger.js
@@ -0,0 +1,35 @@
+// swagger.js 
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+// Swagger definition
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'MINDBRIDGE API DOCUMENTATION',
+    version: '1.0.0',
+    description: 'This is the MindBridge API documentation made with Express and documented with Swagger',
+  },
+  servers: [
+    {
+        url: 'https://backend-production-0710.up.railway.app/v1/api',
+        description: 'Production server',
+      },
+    {
+      url: 'http://localhost:3000/v1/api',
+      description: 'Development server',
+    }
+  ],
+};
+
+// Options for the swagger docs
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./routes/*.js'],
+};
+
+// Initialize swagger-jsdoc
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = { swaggerSpec, swaggerUi };
